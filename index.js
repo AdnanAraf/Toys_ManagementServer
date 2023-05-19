a483d210adf2f585e25225599fde9b01c4c54c36
AdnanAraf <arafadnan10@gmail.com>
2023-05-19 16:49:30 +0600

    Third

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 app.use(cors());
@@ -28,10 +28,26 @@ async function run() {
 
     const ToysCollection = client.db("ToysManagement").collection("ToysData");
 
+    app.get("/ToysData", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await ToysCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/ToysData", async (req, res) => {
       const booking = req.body;
       console.log(booking);
       const result = await ToysCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.delete("/ToysData/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await ToysCollection.deleteOne(query);
       res.send(result);
     });
 
